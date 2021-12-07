f51e6ba39d7d560fd24ffef4615b08a53ed0bec3
Felipe J. Lopez <felipe.lopez@live.com.mx>
2021-12-07 17:02:58 -0600

    Ejercicio de utilizacion de un método simple en C#

---
+++ AreaCirculo/AreaCirculo/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace AreaCirculo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const double pi = 3.1416;
+            Console.WriteLine("Introduce la medida del radio: ");
+            double radio = double.Parse((Console.ReadLine()));
+            //double area = radio * radio * pi;
+            double area = Math.Pow(radio, 2)*pi;
+            Console.WriteLine($"El área del circulo es: {area}");
+
+        }
+    }
+}
---
+++ EjemploMetodos/EjemploMetodos/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Globalization;
+
+namespace EjemploMetodos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Se desarrolla la funcióm
+            int sumaNumeros(int num1, int num2)
+            {
+                int resultado = num1 + num2;
+                return resultado;
+            }
+            
+            //Se desarrolla el programa y despues se manda llamar la función
+            Console.WriteLine("Introduce el primer número: ");
+            int var1 = Int32.Parse(Console.ReadLine());
+            Console.WriteLine("Introduce el segundo número: ");
+            int var2=Int32.Parse(Console.ReadLine());
+            int result = sumaNumeros(var1, var2);
+            Console.WriteLine(result);
+        }
+    }
+}
