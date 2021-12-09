18b684e66e29bbf68c3d748ef6d31686cc06aac7
Felipe J. Lopez <felipe.lopez@live.com.mx>
2021-12-08 21:30:58 -0600

    Programa que utiliza la sobrecarga de métodos para realizar una suma de tres maneras diferentes con el mismo nombre de método.

---
+++ SobrecargaMetodos/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace SobrecargaMetodos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Programa que suma dos numeros a través de sobrecarga de métodos
+            System.Console.WriteLine(Suma(2,3,5));
+            System.Console.WriteLine(Suma(2,3));
+            System.Console.WriteLine(Suma(2.5,3.5));
+
+        }
+
+        // Sobrecarga de métodos
+        public static int Suma(int a, int b)
+        {
+            return a + b;
+        }
+
+        public static double Suma(double a, double b)
+        {
+            return a + b;
+        }
+
+        public static int Suma(int a, int b, int c)
+        {
+            return a + b + c;
+        }
+    }
+}
