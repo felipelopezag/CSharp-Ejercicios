4ba337e6f1d48d74d924e9935f19254e88b5fe98
Felipe J. Lopez <felipe.lopez@live.com.mx>
2021-12-08 22:43:20 -0600

    Pruebas de CSharp sin importancia.

---
+++ Pruebas/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Pruebas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Suma(1, 2));
+        }
+        static int Suma(int v1, int v2)=> v1 + v2;
+    }
+}
