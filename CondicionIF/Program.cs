2570f4345a3bb3b7c95cd1d12e5ade9eb0a87647
Felipe J. Lopez <felipe.lopez@live.com.mx>
2021-12-08 23:56:40 -0600

    Condicional IF

---
+++ CondicionIF/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace CondicionIF
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* Declaración de variable booleana.
+            Al colocar un signo de admiracion hacia abajo invierte el valor original.
+            bool hacefrio=false;
+            Console.WriteLine(!hacefrio);
+            */
+
+            // Condicional que permite saber si una persona es mayor de edad o no.
+            Console.WriteLine("Ingrese su edad: ");
+            int edad = int.Parse(Console.ReadLine());
+            if (edad >= 18)
+            {
+                Console.WriteLine("Usted es mayor de edad.");
+            }
+            else
+            {
+                Console.WriteLine("Usted es menor de edad.");
+            }
+
+        }
+    }
+}
