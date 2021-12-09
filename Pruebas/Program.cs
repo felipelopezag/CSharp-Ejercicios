08f42957ed9d65098ffee2a21e589780f0efc0c1
Felipe J. Lopez <felipe.lopez@live.com.mx>
2021-12-08 22:45:20 -0600

    Sin cambios

--- Pruebas/Program.cs
+++ Pruebas/Program.cs
@@ -8,6 +8,9 @@ namespace Pruebas
         {
             Console.WriteLine(Suma(1, 2));
         }
-        static int Suma(int v1, int v2)=> v1 + v2;
+        static int Suma(int v1, int v2){
+            int v = v1 + v2;
+            return v;
+        }
     }
 }
