5b61d23049b3f9e42fdd964f14a39ae4cc128b9b
Felipe J. Lopez <felipe.lopez@live.com.mx>
2021-12-09 19:03:12 -0600

    Se añadio condicional doble

--- CondicionIF/Program.cs
+++ CondicionIF/Program.cs
@@ -6,12 +6,6 @@ namespace CondicionIF
     {
         static void Main(string[] args)
         {
-            /* Declaración de variable booleana.
-            Al colocar un signo de admiracion hacia abajo invierte el valor original.
-            bool hacefrio=false;
-            Console.WriteLine(!hacefrio);
-            */
-
             // Condicional que permite saber si una persona es mayor de edad o no.
             Console.WriteLine("Ingrese su edad: ");
             int edad = int.Parse(Console.ReadLine());
@@ -22,6 +16,20 @@ namespace CondicionIF
             else
             {
                 Console.WriteLine("Usted es menor de edad.");
+            }
+
+            // Condicional que permite saber si una persona puede votar o no.
+            Console.WriteLine("Ingrese su edad: ");
+            int edad2 = int.Parse(Console.ReadLine());
+            System.Console.WriteLine("Ingrese su sexo: ");
+            string sexo = Console.ReadLine();
+            if (edad2 >= 18 && sexo == "f")
+            {
+                Console.WriteLine("Usted puede votar.");
+            }
+            else
+            {
+                Console.WriteLine("Usted no puede votar.");
             }
 
         }
