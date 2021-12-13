77f892c435e8386e9dfb50fdd2043ee0deec641f
Felipe J. Lopez <felipe.lopez@live.com.mx>
2021-12-13 17:20:18 -0600

    Ejercicio de traductor de dias de la semana de ingles a español.

---
+++ ElseIF/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace ElseIF
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Traductor de dias de la semana de ingles a español");
+            Console.WriteLine("Ingrese el dia de la semana en ingles");
+            string dia = Console.ReadLine();
+            if (dia == "Monday")
+            {
+                Console.WriteLine("Lunes");
+            }
+            else if (dia == "Tuesday")
+            {
+                Console.WriteLine("Martes");
+            }
+            else if (dia == "Wednesday")
+            {
+                Console.WriteLine("Miercoles");
+            }
+            else if (dia == "Thursday")
+            {
+                Console.WriteLine("Jueves");
+            }
+            else if (dia == "Friday")
+            {
+                Console.WriteLine("Viernes");
+            }
+            else if (dia == "Saturday")
+            {
+                Console.WriteLine("Sabado");
+            }
+            else if (dia == "Sunday")
+            {
+                Console.WriteLine("Domingo");
+            }
+            else
+            {
+                Console.WriteLine("Dia invalido");
+            }
+        }
+    }
+}
