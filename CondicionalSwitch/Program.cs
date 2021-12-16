83adbafbe1c58026305c1e92763d38209783e821
Felipe J. Lopez <felipe.lopez@live.com.mx>
2021-12-16 16:35:19 -0600

    Ningun cambio

---
+++ CondicionalSwitch/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace CondicionalSwitch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hola Mundo");
+            Console.WriteLine("Hola Fel");
+        }
+    }
+}
