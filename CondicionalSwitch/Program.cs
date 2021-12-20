05d64c83f296709fe6f6fbd2d3c300c9ac549f1c
Felipe J. Lopez <felipe.lopez@live.com.mx>
2021-12-19 19:46:53 -0600

    Uso de Switch

--- CondicionalSwitch/Program.cs
+++ CondicionalSwitch/Program.cs
@@ -6,8 +6,45 @@ namespace CondicionalSwitch
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hola Mundo");
-            Console.WriteLine("Hola Fel");
+            // Uso de switch para convertir numeros del 1 al 10 a letras
+            System.Console.WriteLine("Ingrese un numero del 1 al 10");
+            int a = int.Parse(Console.ReadLine() ?? throw new InvalidOperationException());
+            switch (a)
+            {
+                case 1:
+                    System.Console.WriteLine("Uno");
+                    break;
+                case 2:
+                    System.Console.WriteLine("Dos");
+                    break;
+                case 3:
+                    System.Console.WriteLine("Tres");
+                    break;
+                case 4:
+                    System.Console.WriteLine("Cuatro");
+                    break;
+                case 5:
+                    System.Console.WriteLine("Cinco");
+                    break;
+                case 6:
+                    System.Console.WriteLine("Seis");
+                    break;
+                case 7:
+                    System.Console.WriteLine("Siete");
+                    break;
+                case 8:
+                    System.Console.WriteLine("Ocho");
+                    break;
+                case 9:
+                    System.Console.WriteLine("Nueve");
+                    break;
+                case 10:
+                    System.Console.WriteLine("Diez");
+                    break;
+                default:
+                    System.Console.WriteLine("Numero fuera de rango");
+                    break;
+            }
         }
     }
 }
