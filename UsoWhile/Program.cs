3301fd9a7bd3be9163b3e0fbd753a5862058dc79
Felipe J. Lopez <felipe.lopez@live.com.mx>
2021-12-24 10:51:50 -0600

    .

---
+++ UsoWhile/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace UsoWhile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Uso del bucle while
+            System.Console.WriteLine("¿Quieres entrar en el bucle while?");
+            string bucle=System.Console.ReadLine();
+            while(bucle!="no")
+            {
+                System.Console.WriteLine("Haz entrado en el bucle while");
+                System.Console.WriteLine("Teclea tu nombre: ");
+                string nombre=System.Console.ReadLine();
+                System.Console.WriteLine($"Hola {nombre} para salir presiona 'no'");
+                bucle=System.Console.ReadLine();
+            }
+            System.Console.WriteLine("Hasta luego");
+        }
+    }
+}
