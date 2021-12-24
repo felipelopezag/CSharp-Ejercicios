d25448212a731dba3784fe17fa1a4beb0cc68120
Felipe J. Lopez <felipe.lopez@live.com.mx>
2021-12-24 11:13:04 -0600

    .

--- AdivinarAleatorio/Program.cs
+++ AdivinarAleatorio/Program.cs
@@ -6,7 +6,22 @@ namespace AdivinarAleatorio
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // Programa que genera un número aleatorio entre 0 y 100 y debes adivinar cuál generó.
+            Random numero= new Random(); //Se crea un objeto que generará un número aleatorio
+            int aleatorio = numero.Next(1,100); // Se crea una variable con un número aleatorio
+            int minumero=101; //Se inicializa la variable en un número fuera del rango de 0 y 100
+            int intentos=0; // Se inicializa la variable intentos en cero
+            System.Console.WriteLine("Introduce un número entre el 0 y 100: ");
+            while(aleatorio!=minumero)
+            {
+                intentos++; // Se incrementa un intento en uno
+                minumero=int.Parse(Console.ReadLine()); // Se almacena y sobre-escribe el número dado por el usuario.
+                
+                //Se valída el número proporcionado con el usuario con el generado aleatoriamente.
+                if(minumero>aleatorio) System.Console.WriteLine("El número es más bajo");
+                if(minumero<aleatorio) System.Console.WriteLine("El número es más alto");
+            }
+            System.Console.WriteLine($"Correcto!, haz necesitado {intentos} intentos para lograrlo");
         }
     }
 }
