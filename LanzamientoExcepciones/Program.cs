a6889629b7ec6dba8b7e677ca73d675fa74ab373
Felipe J. Lopez <felipe.lopez@live.com.mx>
2021-12-26 21:40:33 -0600

    .

---
+++ LanzamientoExcepciones/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+
+namespace LanzamientoExcepciones
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Teclea un número entre el 1 y el 12");
+            int mes=int.Parse(Console.ReadLine());
+            System.Console.WriteLine("El Mes correspondiente es: "+ NombreDelMes(mes));
+        }
+
+        public static string NombreDelMes(int mes)
+        {
+            switch(mes)
+            {
+                case 1:
+                {
+                    return "Enero";
+                }
+                case 2:
+                {
+                    return "Febrero";
+                }
+                case 3:
+                {
+                   return "Marzo";
+                }
+                case 4:
+                {
+                   return "Abril";
+                }
+                case 5:
+                {
+                   return "Mayo";
+                }
+                case 6:
+                {
+                   return "Junio";
+                }
+                case 7:
+                {
+                   return "Julio";
+                }
+                case 8:
+                {
+                   return "Agosto";
+                }
+                case 9:
+                {
+                   return "Septiembre";
+                }
+                case 10:
+                {
+                   return "Octubre";
+                }
+                case 11:
+                {
+                   return "Noviembre";;
+                }
+                case 12:
+                {
+                   return "Diciembre";
+                }
+                default:
+                {
+                    return "Número Inválido";
+                }
+                
+            }
+        }
+    }
+}
---
+++ UsoChecked/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace UsoChecked
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numeromaximo=int.MaxValue;
+            System.Console.WriteLine(numeromaximo);
+
+            /* Esta función verifica el desbordamiento aritmetico (sobrecarga), 
+            puede activarse en VS en propiedades del proyecto */
+            checked{ 
+            int sobrecarga=numeromaximo+20;
+            System.Console.WriteLine(sobrecarga);
+            }
+        }
+    }
+}
