0b32b382a7adf5b69f8c7668dcee317194382f59
Felipe J. Lopez <felipe.lopez@live.com.mx>
2022-01-02 19:57:50 -0600

    .

---
+++ EjemplosPOO/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace EjemplosPOO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Circulo miCirculo; //Creación de objeto de tipo circulo
+            miCirculo=new Circulo(); //Iniciación de variable/objeto de tipo circulo. Instanciar una clase.
+            //Tambien se le llama instanciación / EJemplarización / Creación de ejemplar de clase.
+
+            System.Console.WriteLine(miCirculo.CalculoArea(5));
+        }
+    }
+
+    class Circulo
+    {
+        const double pi=3.141592; // Propiedad de la clase circulo. Campo de clase.
+        public double CalculoArea(double radio) //Método de clase ¿Que pueden hacer los objetos de tipo "circulo"
+        {
+            double area=pi*Math.Pow(radio,2);
+            return area;
+        }
+    }
+}
