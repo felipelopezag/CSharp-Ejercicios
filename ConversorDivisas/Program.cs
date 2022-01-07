c02da6fe10d91e4358e7ea3fb611cfd74695a35b
Felipe J. Lopez <felipe.lopez@live.com.mx>
2022-01-07 17:49:24 -0600

    Conversor de Pesos a otras divisas mediante objetos

---
+++ ConversorDivisas/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace ConversorDivisas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Programa que convierte divisas mediante objetos
+            Conversor PesoA;
+            PesoA=new Conversor();
+            Console.WriteLine("Teclea la cantidad en pesos mexicanos: $");
+            double pesos=double.Parse(Console.ReadLine());
+            double dolares=PesoA.Dolar(pesos);
+            double euros=PesoA.Euro(pesos);
+            double yenes=PesoA.Yen(pesos);
+            double libras=PesoA.LibraUK(pesos);
+            System.Console.WriteLine("Equivale a "+dolares+" dolares");
+            System.Console.WriteLine("Equivale a "+euros+" euros");
+            System.Console.WriteLine("Equivale a "+yenes+" yenes");
+            System.Console.WriteLine("Equivale a "+libras+" libras");
+        }
+
+        public class Conversor
+        {
+            public double Dolar(double cantidad)
+            {
+                double resultado=cantidad*0.049;
+                return resultado;
+            }
+
+            public double Euro(double cantidad)
+            {
+                double resultado=cantidad*0.043;
+                return resultado;
+            }
+
+            public double Yen(double cantidad)
+            {
+                double resultado=cantidad*5.67;
+                return resultado;
+            }
+
+            public double LibraUK(double cantidad)
+            {
+                double resultado=cantidad*0.036;
+                return resultado;
+            }
+        }
+    }
+}
